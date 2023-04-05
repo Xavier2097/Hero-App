d816b63e02b6fad11c2c8abe7e3b8fedfcc7ad61
Xavier Puentestar <xavierpuentestar97@gmail.com>
2023-04-04 23:57:39 -0500

    app terminada

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,1 +1,3 @@
 export * from './pagues';
+export * from './context';
+
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,33 @@
 import { Route, Routes } from "react-router-dom";
 import { HeroesRoutes } from "../heroes";
-import { Login } from "../auth";
+import { LoginPague } from "../auth";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 
 
 export const AppRouter = () => {
   return (
     <>
-    
-    <Routes>
-        
-        <Route path="login" element={<Login/>} />
-        <Route path="/*" element={<HeroesRoutes/>} />
 
-        
-    </Routes>
+      <Routes>
+
+       
+
+        <Route path="login" element={<LoginPague />} />
+
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        }
+        />
+
+        {/*<Route path="/*" element={<HeroesRoutes/>} /> */}
+
+
+      </Routes>
 
     </>
   )
